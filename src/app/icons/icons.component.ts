4d5780125899256ae1b295517e9f92ab985e7a17
El Ech Iheb <ihebelleuch6@gmail.com>
2024-02-19 12:37:13 +0100

    New Commit
    19/02/2024

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -11,5 +11,11 @@ export class IconsComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  menuItems = [
+    { title: "Toutes les demandes", link: "#" },
+    { title: "Rechercher une demande", link: "#" },
+    { title: "Extraire la demande (CSV)", link: "#" },
+    { title: "Demander plus de détails", link: "#" },
+    { title: "Liste des réclamations", link: "#" }
+  ];
 }
